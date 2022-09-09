40b224266f57536749050e48563b6481170f19ff
Mike <mike@nowhere.com>
2022-09-09 15:38:23 +0200

    Restore point

--- dev/construct_primes.py
+++ dev/construct_primes.py
@@ -28,6 +28,8 @@ import Primes
 '''
 	Prime.isprime() handles approx 100,000 queries/sec
 	
+	Note: search for maximum number of repeated digits - not longest run
+	
 	consider 4 digit primes 1000-9999
 	repeated digit d = 0	not msd or lsd
 	repeated digit 2,4,5,6,8	not prime if lsd
@@ -48,27 +50,37 @@ import Primes
 	
 '''
 
-def rot_right(n,width):
-	d = n % 10
-	n //= 10
-	n = n + d * (10**(width-1))
-	return n
+def rot_right(m,width):
+	d = m % 10
+	m //= 10
+	m = m + d * (10**(width-1))
+	return m
+	
+def rot_left(m,width):
+	d = m // 10**(width-1)	#MSD
+	m %= 10**(width-1)	# remaining digits
+	m = m*10 + d
+	return m
 
 def prime_runs(drep, width):
 	# Construct a list of primes with runs of digits
-	# list  of variable numbers in tuples indexed by the rep digit 0-9
-	n_var = [(0),(3,7,9),(1,3,7,9),(1,7),(1,3,7,9),(1,3,7,9),(1,7,9),(1,3,9),(1,3,7,9),(1,7)]
-	for var_d in n_var[drep]: # e.g. drep = 3 =>(1,7,9)
-		s = str(drep)*(width-1) + str(var_d)
+	# list  of variable numbers in tuples indexed by the rep digit 0-9	
+	n_var = (9,7,3,1)
+	for var_d in n_var:
+		s =  str(var_d) + str(drep)*(width-1)
 		n = int(s)
 		for x in range(width):
-			n = rot_right(n,width)
-			print(n, Primes.isprime(n))
+			print(n,end=" ")
+			if Primes.isprime(n):
+				print("Prime")
+			else:
+				print("Comp")
+			n = rot_left(n,width)
 		print()
 			
 def main(args):
-	prime_runs(2,4)
-
+	prime_runs(9,4)
+	
 if __name__ == '__main__':
     import sys
     sys.exit(main(sys.argv))
